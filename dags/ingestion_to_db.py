9e6d1dffd5f9fbd26632921d91f9cd2eec09fbe7
martinalon <martinalonsofloresgonzalez@gmail.com>
2022-07-19 23:07:33 -0500

    fixing typo

--- dags/ingestion_to_db.py
+++ dags/ingestion_to_db.py
@@ -2,10 +2,9 @@ from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
-with DAG('db_ingestion', start_date = days_ago(1)) as dag
-    start_workflow = DummyOperator(task_id = 'start_workflow')
-    validate = DummyOperator(task_id = 'validate')
-    prepare = DummyOperator(task_id = 'prepare')
-    load = DummyOperator(task_id = 'load')
-    end_workflow = DummyOperator(task_id = 'end_workflow')
-
+with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+    start_workflow = DummyOperator(task_id="start_workflow")
+    validate = DummyOperator(task_id="validate")
+    prepare = DummyOperator(task_id="prepare")
+    load = DummyOperator(task_id="load")
+    end_workflow = DummyOperator(task_id="end_workflow")
