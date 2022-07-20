8b9afaed4f470be4af4dc1a624fd464059c88c96
martinalon <martinalonsofloresgonzalez@gmail.com>
2022-07-19 23:53:22 -0500

    adding the conection to a host in aws

--- dags/ingestion_to_db.py
+++ dags/ingestion_to_db.py
@@ -2,19 +2,10 @@ import datetime
 
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 
-with DAG("db_ingestion", start_date=datetime.date(2022, 1, 1)) as dag:
-    start_workflow = DummyOperator(task_id="start_workflow")
-    validate = DummyOperator(task_id="validate")
-    prepare = DummyOperator(task_id="prepare")
-    load = DummyOperator(task_id="load")
-    end_workflow = DummyOperator(task_id="end_workflow")
-
-    start_workflow >> validate >> prepare >> load >> end_workflow
-
-
-# with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+# with DAG("db_ingestion", start_date=datetime.date(2022, 1, 1)) as dag:
 #    start_workflow = DummyOperator(task_id="start_workflow")
 #    validate = DummyOperator(task_id="validate")
 #    prepare = DummyOperator(task_id="prepare")
@@ -22,3 +13,17 @@ with DAG("db_ingestion", start_date=datetime.date(2022, 1, 1)) as dag:
 #    end_workflow = DummyOperator(task_id="end_workflow")
 
 #    start_workflow >> validate >> prepare >> load >> end_workflow
+
+
+with DAG(
+    "db_ingestion", start_date=days_ago(1), schedule_interval="@once"
+) as dag:
+    start_workflow = DummyOperator(task_id="start_workflow")
+    validate = DummyOperator(task_id="validate")
+    prepare = PostgresOperator(
+        task_id="prepare", postgres_conn_id="ml_conn", sql=""
+    )
+    load = DummyOperator(task_id="load")
+    end_workflow = DummyOperator(task_id="end_workflow")
+
+    start_workflow >> validate >> prepare >> load >> end_workflow
