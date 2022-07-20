7edf8ad2d702cc7f80769c78e424023a3f245b3b
martinalon <martinalonsofloresgonzalez@gmail.com>
2022-07-19 23:20:35 -0500

    changing time

--- dags/ingestion_to_db.py
+++ dags/ingestion_to_db.py
@@ -1,8 +1,10 @@
+import datetime
+
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
-with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+with DAG("db_ingestion", start_date=datetime.date(2022, 1, 1)) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
     validate = DummyOperator(task_id="validate")
     prepare = DummyOperator(task_id="prepare")
@@ -10,3 +12,13 @@ with DAG("db_ingestion", start_date=days_ago(1)) as dag:
     end_workflow = DummyOperator(task_id="end_workflow")
 
     start_workflow >> validate >> prepare >> load >> end_workflow
+
+
+# with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+#    start_workflow = DummyOperator(task_id="start_workflow")
+#    validate = DummyOperator(task_id="validate")
+#    prepare = DummyOperator(task_id="prepare")
+#    load = DummyOperator(task_id="load")
+#    end_workflow = DummyOperator(task_id="end_workflow")
+
+#    start_workflow >> validate >> prepare >> load >> end_workflow
